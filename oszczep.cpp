f06d4d6ad48be502c3da4155bd0811f47fbb5fc9
komp <kuba-swislocki5@outlook.com>
2016-11-17 21:49:59 +0100

    no message

---
+++ oszczep.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"podaj liczbe wynikow"<<endl;
+    cin>>n;
+    for (int a=0;a<n;a++)
+    {
+
+    }
+}
