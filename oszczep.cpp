34ef5b7f917440e68cb72ffaaa2ee5cb8e98cfbe
komp <kuba-swislocki5@outlook.com>
2016-11-17 21:56:57 +0100

    no message

--- oszczep.cpp
+++ oszczep.cpp
@@ -9,8 +9,12 @@ int main()
     int n;
     cout<<"podaj liczbe wynikow"<<endl;
     cin>>n;
+    int wyniki[n];
+    cout<<"podaj wyniki po jednym"<<endl;
     for (int a=0;a<n;a++)
     {
-
+        cin>>wyniki[a];
     }
+    sort(wyniki,wyniki+8);
+    cout<<"najlepszy wynik tego zawodnika to "<<wyniki[n-1]<<endl;
 }
